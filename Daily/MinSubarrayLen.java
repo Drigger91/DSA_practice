b9e8cffa1e25d8ac6edfdd9d4a052a428b140850
Drigger91 <82764734+Drigger91@users.noreply.github.com>
2023-07-06 23:44:17 +0530

    Min Subarray length 6 July 23

---
+++ Daily/MinSubarrayLen.java
@@ -0,0 +1,25 @@
+package DSA_practice.Daily;
+
+public class MinSubarrayLen {
+    public static void main(String[] args) {
+        int[] arr = {2,3,1,2,4,3};
+        int target = 7;
+        System.out.println(minSubArrayLen(target, arr));
+    }
+    public static int minSubArrayLen(int target, int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int sum = 0;
+        int start = 0;
+        int end = 0;
+        while(end < nums.length){
+            sum += nums[end];
+            while(sum >= target){
+                min = Math.min(min, end - start + 1);
+                sum -= nums[start];
+                start++;
+            }
+            end++;
+        }
+        return min == Integer.MAX_VALUE ? 0 : min;
+    }
+}
